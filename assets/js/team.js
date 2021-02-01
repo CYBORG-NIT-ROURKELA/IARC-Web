54839671039ad2a7eae9ca13bc58ea19483987a9
Ashutosh Nayak <nayakashutosh99@gmail.com>
2021-02-01 14:09:53 +0530

    team page done

--- assets/js/team.js
+++ assets/js/team.js
@@ -1246,42 +1246,45 @@ function(t, e) {
 
 
 
+
+
+
+
 var $grid = $('.grid').isotope({
-  itemSelector: '.element-item'
+  itemSelector: '.color-shape'
 });
 
 // store filter for each group
 var filters = {};
 
-$('.filters').on('click', '.button', function() {
-  var $this = $(this);
+$('.filters').on( 'click', '.button', function( event ) {
+  var $button = $( event.currentTarget );
   // get group key
-  var $buttonGroup = $this.parents('.button-group');
+  var $buttonGroup = $button.parents('.button-group');
   var filterGroup = $buttonGroup.attr('data-filter-group');
   // set filter for group
-  filters[filterGroup] = $this.attr('data-filter');
+  filters[ filterGroup ] = $button.attr('data-filter');
   // combine filters
-  var filterValue = concatValues(filters);
+  var filterValue = concatValues( filters );
   // set filter for Isotope
-  $grid.isotope({
-    filter: filterValue
-  });
+  $grid.isotope({ filter: filterValue });
 });
 
 // change is-checked class on buttons
-$('.button-group').each(function(i, buttonGroup) {
-  var $buttonGroup = $(buttonGroup);
-  $buttonGroup.on('click', 'button', function() {
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function( event ) {
     $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $(this).addClass('is-checked');
+    var $button = $( event.currentTarget );
+    $button.addClass('is-checked');
   });
 });
 
 // flatten object by concatting values
-function concatValues(obj) {
+function concatValues( obj ) {
   var value = '';
-  for (var prop in obj) {
-    value += obj[prop];
+  for ( var prop in obj ) {
+    value += obj[ prop ];
   }
   return value;
 }
